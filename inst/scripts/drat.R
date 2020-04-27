7c115675ce338f441911fd645f23cdfa33a95bde
Richard White <w@rwhite.no>
2020-04-27 13:01:54 +0200

    drat

--- inst/scripts/drat.R
+++ inst/scripts/drat.R
@@ -2,11 +2,11 @@ devtools::build(binary=F)
 devtools::build(binary=T)
 
 drat::insertPackage(
-  file = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/packages/MOMO_2020.4.17.tar.gz",
+  file = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/euromomonetwork/MOMO_2020.4.27.tar.gz",
   repodir = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/euromomonetwork/drat"
 )
 
 drat::insertPackage(
-  file = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/packages/MOMO_2020.4.17.zip",
+  file = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/euromomonetwork/MOMO_2020.4.27.zip",
   repodir = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/euromomonetwork/drat"
 )
