84683cfde6b0b516cb5824c4e3340e850132417f
Richard White <w@rwhite.no>
2020-04-27 12:57:40 +0200

    include drat script

---
+++ inst/scripts/drat.R
@@ -0,0 +1,12 @@
+devtools::build(binary=F)
+devtools::build(binary=T)
+
+drat::insertPackage(
+  file = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/packages/MOMO_2020.4.17.tar.gz",
+  repodir = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/euromomonetwork/drat"
+)
+
+drat::insertPackage(
+  file = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/packages/MOMO_2020.4.17.zip",
+  repodir = "C:/Users/riwh/OneDrive - Folkehelseinstituttet/git/euromomonetwork/drat"
+)
